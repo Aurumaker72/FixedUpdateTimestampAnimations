53e78063045dc4d1126b2403b60715aff0f154a4
Alex <48759429+Aurumaker72@users.noreply.github.com>
2022-08-09 12:16:00 +0200

    Add cubic interpolator

---
+++ Interpolators/EasedCubicInterpolator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixedUpdateTimestampAnimations.Interpolators
+{
+    public class EasedCubicInterpolator : IInterpolator
+    {
+        /// <summary>
+        /// Returns an eased cubic interpolated value between v1 and v2, determined by t
+        /// </summary>
+        /// <param name="v1">The lower bound</param>
+        /// <param name="v2">The higher bound</param>
+        /// <param name="t">The progress/tempus</param>
+        /// <returns>The scaled value</returns>
+        public float Interpolate(float v1, float v2, float t)
+        {
+            return MathHelper.Remap((float)(t < 0.5 ? 4 * t * t * t : 1 - Math.Pow(-2 * t + 2, 3) / 2), 0, 1, v1, v2);
+        }
+    }
+}
